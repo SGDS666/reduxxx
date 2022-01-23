027ca08ea38e13a033567398fc5db24fe8ca9ed5
zhangsen <1226931269@qq.com>
2022-01-23 09:37:20 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-// import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/jsx-pascal-case */
+
+import './app.scss'
+
 
 function App() {
+  
+
   return (
     <div className="App">
-      app
+     app
     </div>
   );
 }
